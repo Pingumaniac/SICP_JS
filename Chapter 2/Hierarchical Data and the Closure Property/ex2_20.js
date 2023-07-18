b0d0b7812175b7c22a0365211f472a5c4923150a
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-07-18 16:11:20 -0500

    Update ex2_20.js

--- Chapter 2/Hierarchical Data and the Closure Property/ex2_20.js
+++ Chapter 2/Hierarchical Data and the Closure Property/ex2_20.js
@@ -8,7 +8,7 @@ const tail = (items) => { return items === null ? null : items.right; }
 const list = (...items) => { return items.length === 0 ? null : new Pair(items[0], list(...items.slice(1))); }
 const append = (list1, list2) => { return list1 === null ? list2 : new Pair(head(list1), append(tail(list1), list2)); }
 const reverse = (items) => { return items === null ? null : append(reverse(tail(items)), list(head(items))); }
-const printList = (list) => { return list === null ? '' : head(list) + ' ' + printList(tail(list)); }
+const print_list = (list) => { return list === null ? '' : head(list) + ' ' + print_list(tail(list)); }
 
 function plus_curried(x) { return y => x + y; }
 
