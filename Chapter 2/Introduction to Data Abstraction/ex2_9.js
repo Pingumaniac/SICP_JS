6d60902032568dc912c7db7f238e44fbd3cfa76a
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-07-18 12:58:54 -0500

    Add files via upload

---
+++ Chapter 2/Introduction to Data Abstraction/ex2_9.js
@@ -0,0 +1,11 @@
+/*
+sum of width of a and b = sum of lower bounds of a and b + sum of upper bounds of a and b
+
+subtration of width from a by b
+= subtraction of the lower bounds from a by b + subtraction of the upper bounds from a by b
+
+for multiplication and division
+e.g. if a = [-2, 2] and b = [-1, 1]
+
+the width does not change after multiplication or division
+*/
