62abf1301f47a984563357a429f4aa06555bcf2b
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-07-20 13:11:26 -0500

    Add files via upload

---
+++ Chapter 2/Symbolic Data/ex2_71.js
@@ -0,0 +1,36 @@
+/*
+when n = 5, the tree is:
+         31
+        /  \
+       15   16
+      /  \
+     7    8
+    / \
+   3   4
+  / \
+ 1   2
+
+when n = 10, the tree is:
+
+                  511
+                 /   \
+                255  256
+               /   \
+             127   128
+            /   \
+           63   64
+          /  \
+         31  32
+        /  \
+       15  16
+      /  \
+     7    8
+    / \
+   3   4
+  / \
+ 1   2
+
+
+Most frequent symbol: 1 bit
+Least frequent symbol: n bits
+*/
---
+++ Chapter 2/Symbolic Data/ex2_72.js
@@ -0,0 +1,4 @@
+/*
+Order of growth of the number of steps needed to encode a most frequent symbol: O(1)
+Order of growth of the number of steps needed to encode a least frequent symbol: O(n)
+*/
