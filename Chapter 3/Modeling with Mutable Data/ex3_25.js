9d495cf00d0d62fa894e4a7d13ca3423a4c877c2
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-07-23 21:16:52 -0500

    Add files via upload

---
+++ Chapter 3/Modeling with Mutable Data/ex3_25.js
@@ -0,0 +1,36 @@
+function make_multi_key_table(key = null, value = null) {
+    return {
+        key: key,
+        value: value,
+        children: [],
+        put: function(keys, value) {
+            if (keys.length === 0) {
+                this.value = value;
+                return;
+            }
+            let child = this.children.find(node => node.key === keys[0]);
+            if (!child) {
+                child = make_multi_key_table(keys[0]);
+                this.children.push(child);
+            }
+            child.put(keys.slice(1), value);
+        },
+        get: function(keys) {
+            if (keys.length === 0) { return this.value; }
+            const child = this.children.find(node => node.key === keys[0]);
+            if (!child) { return null; }
+            return child.get(keys.slice(1));
+        }
+    }
+}
+
+const table = make_multi_key_table();
+
+table.put(["i", "j", "k"], "jfla is pretty");
+console.log(table.get(["i", "j", "k"])); // jfla is pretty
+table.put(["i", "j"], "jfla is a singer");
+console.log(table.get(["i", "j"])); // jfla is a singer
+console.log(table.get(["i"])); // null (no value directly stored under "a")
+table.put(["i"], "jfla");
+console.log(table.get(["i"]));  // jfla
+
