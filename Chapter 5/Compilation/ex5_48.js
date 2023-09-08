b95f4bbca725e9896443c09f0a3f744f4c34cbb6
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-09-07 20:03:54 -0500

    Add files via upload

---
+++ Chapter 5/Compilation/ex5_48.js
@@ -0,0 +1,33 @@
+/*
+Part (a)
+
+compiled:
+
+-----------------------------------
+| n | no of pushes | maximum depth|
+|---------------------------------|
+| 2 |      13      |      5       |
+|---------------------------------|
+| 3 |      19      |      8       |
+|---------------------------------|
+| 4 |      25      |      11      |
+|---------------------------------|
+| 5 |      31      |      14      |
+-----------------------------------
+
+Hence
+Formula for no of pushes = 6n + 1
+Formula for maximum depth is 3n - 1.
+
+Ratio for no of pushes:
+compiled vs interpretation: 6 / 32
+special-purpose vs interpretation: 2 /32
+
+Ratio for maximum depth:
+compiled vs interpretation: 3 / 5
+special-purpose vs interpretation: 2 / 5
+
+
+Part (b)
+Perhaps incorporate machine learning for compiler optimization.
+*/
